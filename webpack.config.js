84cb7e8483effcb738f3091fef92082f359bb8e2
Sam Mauer <sam.mauer@gmail.com>
2019-02-12 22:18:56 -0600

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    entry: __dirname + "/src/index.js",
+    output: {
+        path: __dirname + "/public",
+        filename: "bundle.js",
+        publicPath: "/",
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015','react'],
+                    plugins: ['transform-class-properties']
+                }
+            },
+        ]
+    },
+    devServer: {
+        contentBase: "./public",
+        historyApiFallback: true,
+        inline: true,
+    }
+}
