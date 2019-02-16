d774a8ecca910850283c4ce9f32838c2013aa908
Sam Mauer <sam.mauer@gmail.com>
2019-02-16 12:07:13 -0600

    Finished Ch4, auth and login

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,18 @@ import './app.css';
 import LoginContainer from './LoginContainer';
 
 class App extends Component {
+    state = { user: null };
+
+    componentDidMount() {
+        firebase
+            .auth()
+            .onAuthStateChanged((user) => {
+                if(user) {
+                    this.setState({user});
+                }
+            });
+    }
+
     render() {
         return (
             <div id="container" className="inner-container">
